92ea065c4e19855769a69652fa6e2a5a9230126e
nicohnavarro <nicohnavarro@gmail.com>
2021-01-15 01:18:35 -0300

    :sparkles: add more sidebar options

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,19 @@
 import React from 'react'
+
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+
 const Sidebar = () => {
     return (
         <div className="sidebar">
@@ -15,7 +27,16 @@ const Sidebar = () => {
                 </div>
                 <CreateIcon/>
             </div>
-
+            <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
+            <SidebarOption title="Youtube"/>
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+            <SidebarOption Icon={DraftsIcon} title="Saved items"/>
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+            <SidebarOption Icon={AppsIcon} title="Apps"/>
+            <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+            <hr/>
+            <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
         </div>
     )
 }
