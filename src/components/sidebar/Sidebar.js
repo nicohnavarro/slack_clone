0c62937fa00183558c7cd37d693b3132caa6c60f
nicohnavarro <nicohnavarro@gmail.com>
2021-01-17 17:41:50 -0300

    :fire: add firebase to the project

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -10,6 +10,8 @@ import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import AppsIcon from "@material-ui/icons/Apps";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
@@ -35,8 +37,11 @@ const Sidebar = () => {
             <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
             <SidebarOption Icon={AppsIcon} title="Apps"/>
             <SidebarOption Icon={FileCopyIcon} title="File browser"/>
-            <hr/>
             <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+            <hr/>
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+            <hr/>
+            <SidebarOption Icon={AddIcon} title="Add Channel"/>
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDsmBWlUW8Xu2KLjf1lbyEOAKEvdoJwubc",
+    authDomain: "nicohnavarro-clones.firebaseapp.com",
+    projectId: "nicohnavarro-clones",
+    storageBucket: "nicohnavarro-clones.appspot.com",
+    messagingSenderId: "830974104291",
+    appId: "1:830974104291:web:8efe271505815f4f46ebd6",
+    measurementId: "G-EF3DG6RKJS"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  export {auth,provider};
+  export default db;
