7315d0a9bea9da5e577a45cf235254c9ab4e3ad8
nicohnavarro <nicohnavarro@gmail.com>
2021-01-26 02:02:06 -0300

    :rocket: ready to deploy

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,11 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import db from '../../firebase';
 import Message from './message/Message';
+import ChatInput from './chatInput/ChatInput';
 
 const Chat = () => {
     const { roomId } = useParams();
@@ -16,17 +17,17 @@ const Chat = () => {
             db.collection('rooms').doc(roomId)
                 .onSnapshot((snapshot) => (
                     setRoomDetails(snapshot.data())
-        ))
+                ))
         }
 
         db.collection('rooms').doc(roomId)
-        .collection('messages')
-        .orderBy('timestamp','asc')
-        .onSnapshot((snapshot)=>
-            setRoomMessages(snapshot.docs.map(doc => doc.data()))
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+            .onSnapshot((snapshot) =>
+                setRoomMessages(snapshot.docs.map(doc => doc.data()))
             )
     }, [roomId])
-console.log(roomMessages)
+    console.log(roomMessages)
     return (
         <div className='chat'>
             <div className='chat__header'>
@@ -34,24 +35,26 @@ console.log(roomMessages)
                     <h4 className='chat__channelName'><strong>#{roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon />
                     </h4>
-            </div>
-            <div className='chat__headerRight'>
-                <p>
-                    <InfoOutlinedIcon />Details
+                </div>
+                <div className='chat__headerRight'>
+                    <p>
+                        <InfoOutlinedIcon />Details
                 </p>
+                </div>
             </div>
-        </div>
 
-        <div className="chat__messages">
-            {roomMessages.map(({message, timestamp, user, userImage}) =>(
-                <Message 
-                message= {message}
-                timestamp={timestamp}
-                user={user}
-                userImage={userImage}
-                />
-            ))}
+            <div className="chat__messages">
+                {roomMessages.map(({ message, timestamp, user, userImage }) => (
+                    <Message
+                        message={message}
+                        timestamp={timestamp}
+                        user={user}
+                        userImage={userImage}
+                    />
+                ))}
             </div>
+
+            <ChatInput channelName={roomDetails?.name} channelId={roomId}/>
         </div>
     );
 }
---
+++ src/components/chat/chatInput/ChatInput.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './ChatInput.css';
+import db from '../../../firebase';
+import { useStateValue } from '../../../StateProvider';
+import { Button } from '@material-ui/core';
+import firebase from 'firebase';
+
+const ChatInput = ({channelName, channelId }) => {
+
+    const [input, setInput] = useState('');
+    const [{ user }] = useStateValue();
+
+    const sendMessage = e =>{
+        e.preventDefault();
+        if(channelId){
+            db.collection('rooms').doc(channelId).collection('messages').add({
+                message: input,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                user: user.displayName,
+                userImage: user.photoURL,
+            });
+            setInput('');
+        }
+    }
+    return (
+        <div className='chatInput'>
+            <form>
+                <input
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                placeholder={`Message #${channelName}`}/>
+                <Button type="submit" onClick={sendMessage}>SEND</Button>
+            </form>
+        </div>
+    )
+}
+
+export default ChatInput
