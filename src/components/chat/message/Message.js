f50ab59fa67bcc9c02d98a036af8e38fd786bd33
nicohnavarro <nicohnavarro@gmail.com>
2021-01-25 23:43:09 -0300

    :construction: message component

--- src/components/chat/message/Message.js
+++ src/components/chat/message/Message.js
@@ -6,7 +6,7 @@ const Message = ({message,timestamp,user, userImage}) => {
             <img src={userImage} alt=""/>
             <div className="message__info">
                 <h4>
-                    {user} timestaamps...
+                    {user} <span className='message__timestamp'>{new Date(timestamp?.toDate()).toUTCString()}</span>
                 </h4>
                 <p>{message}</p>
             </div>
